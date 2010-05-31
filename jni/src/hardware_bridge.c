bef6ebb9eb3e3ac97d7852e280312427a7b33a68
Jingnan Si (jingnan si <Jingnan Si (jingnan.si@gmail.com)>
2010-05-31 14:14:23 +0800

    using multi-thread to handling fraemwork request

--- jni/src/hardware_bridge.c
+++ jni/src/hardware_bridge.c
@@ -30,16 +30,18 @@ int enable_debug = 0;
 
 int bootstrap = 0;
 
-struct st_framework_sock {
-	struct st_framework_sock * next;
-	int fw_sock;
-};
+//struct st_framework_sock {
+//	struct st_framework_sock * next;
+//	int fw_sock;
+//};
+
+static void * handle_framework_sock_func(void * lpParam);
 
 int process_framework_command(int fw_socket);
 
 int main(int argc, char ** argv) {
 	int door_sock = -1;
-	struct st_framework_sock * header = NULL;
+	//struct st_framework_sock * header = NULL;
 
 	LOGI("Hardware Bridge Daemon version %d.%d", ver_major, ver_minor);
 
@@ -78,8 +80,8 @@ int main(int argc, char ** argv) {
 		struct timeval to;
 		int max = 0;
 		int rc = 0;
-		struct st_framework_sock * tmp_sock;
-		struct st_framework_sock * prev_sock;
+		//		struct st_framework_sock * tmp_sock;
+		//		struct st_framework_sock * prev_sock;
 
 		to.tv_sec = (60 * 60);
 		to.tv_usec = 0;
@@ -89,13 +91,13 @@ int main(int argc, char ** argv) {
 		if (door_sock > max)
 			max = door_sock;
 
-		tmp_sock = header;
-		while (tmp_sock != NULL) {
-			FD_SET(tmp_sock->fw_sock, &read_fds);
-			if (tmp_sock->fw_sock > max)
-				max = tmp_sock->fw_sock;
-			tmp_sock = tmp_sock->next;
-		}
+		//		tmp_sock = header;
+		//		while (tmp_sock != NULL) {
+		//			FD_SET(tmp_sock->fw_sock, &read_fds);
+		//			if (tmp_sock->fw_sock > max)
+		//				max = tmp_sock->fw_sock;
+		//			tmp_sock = tmp_sock->next;
+		//		}
 
 		if ((rc = select(max + 1, &read_fds, NULL, NULL, &to)) < 0) {
 			LOGE("select() failed (%s)", strerror(errno));
@@ -111,6 +113,8 @@ int main(int argc, char ** argv) {
 			struct sockaddr addr;
 			socklen_t alen;
 			int fw_sock;
+			pthread_t thread_id;
+			int t_error;
 
 			alen = sizeof(addr);
 
@@ -119,75 +123,92 @@ int main(int argc, char ** argv) {
 						errno));
 			}
 
-			tmp_sock = (struct st_framework_sock *)malloc(
-					sizeof(struct st_framework_sock));
-			tmp_sock->fw_sock = fw_sock;
-			tmp_sock->next = header;
-			header = tmp_sock;
+			t_error = pthread_create(&thread_id, NULL,
+					handle_framework_sock_func, (void*)fw_sock);
 
-			LOGD("Accepted connection from framework");
+			//			tmp_sock = (struct st_framework_sock *)malloc(
+			//					sizeof(struct st_framework_sock));
+			//			tmp_sock->fw_sock = fw_sock;
+			//			tmp_sock->next = header;
+			//			header = tmp_sock;
+
+			LOGD("Accepted connection from framework,%d,%d", (int)thread_id, t_error);
 		}
 
-		tmp_sock = header;
-		prev_sock = NULL;
-		while (tmp_sock != NULL) {
-			if (FD_ISSET(tmp_sock->fw_sock, &read_fds)) {
-				if ((rc = process_framework_command(tmp_sock->fw_sock)) < 0) {
-					if (rc == -ECONNRESET) {
-						LOGE("Framework disconnected");
-						close(tmp_sock->fw_sock);
-
-						if (prev_sock == NULL)
-							header = tmp_sock->next;
-						else
-							prev_sock->next = tmp_sock->next;
-
-						free(tmp_sock);
-
-						if (prev_sock == NULL)
-							tmp_sock = header;
-						else
-							tmp_sock = prev_sock->next;
-					} else {
-						LOGE("Error processing framework command (%s)",
-								strerror(errno));
-					}
-				}
-			} else {
-				prev_sock = tmp_sock;
-				tmp_sock = tmp_sock->next;
-			}
-		}
+		//		tmp_sock = header;
+		//		prev_sock = NULL;
+		//		while (tmp_sock != NULL) {
+		//			if (FD_ISSET(tmp_sock->fw_sock, &read_fds)) {
+		//				if ((rc = process_framework_command(tmp_sock->fw_sock)) < 0) {
+		//					if (rc == -ECONNRESET) {
+		//						LOGE("Framework disconnected");
+		//						close(tmp_sock->fw_sock);
+		//
+		//						if (prev_sock == NULL)
+		//							header = tmp_sock->next;
+		//						else
+		//							prev_sock->next = tmp_sock->next;
+		//
+		//						free(tmp_sock);
+		//
+		//						if (prev_sock == NULL)
+		//							tmp_sock = header;
+		//						else
+		//							tmp_sock = prev_sock->next;
+		//					} else {
+		//						LOGE("Error processing framework command (%s)",
+		//								strerror(errno));
+		//					}
+		//				}
+		//			} else {
+		//				prev_sock = tmp_sock;
+		//				tmp_sock = tmp_sock->next;
+		//			}
+		//		}
 
 	} // while
 
 }
 
-//int send_msg(char* message) {
-//	int result = -1;
-//
-//	pthread_mutex_lock(&write_mutex);
-//
-//	LOGD("send_msg(%s):", message);
-//
-//	if (fw_sock >= 0)
-//		result = write(fw_sock, message, strlen(message) + 1);
-//
-//	pthread_mutex_unlock(&write_mutex);
-//
-//	return result;
-//}
-//
-//int send_msg_with_data(char *message, char *data) {
-//	int result = -1;
-//
-//	char* buffer = (char *) alloca(strlen(message) + strlen(data) + 1);
-//	if (!buffer) {
-//		LOGE("alloca failed in send_msg_with_data");
-//		return result;
-//	}
-//
-//	strcpy(buffer, message);
-//	strcat(buffer, data);
-//	return send_msg(buffer);
-//}
+static void * handle_framework_sock_func(void * lpParam) {
+	int rc;
+	int fw_sock;
+
+	fw_sock = (int) lpParam;
+	while (1) {
+		fd_set read_fds;
+		struct timeval to;
+		int max = 0;
+
+		to.tv_sec = (60 * 60);
+		to.tv_usec = 0;
+
+		FD_ZERO(&read_fds);
+		FD_SET(fw_sock, &read_fds);
+
+		if (fw_sock > max)
+			max = fw_sock;
+
+		if ((rc = select(max + 1, &read_fds, NULL, NULL, &to)) < 0) {
+			LOGE("select() failed (%s)", strerror(errno));
+			sleep(1);
+			continue;
+		}
+
+		if (!rc) {
+			continue;
+		}
+
+		if ((rc = process_framework_command(fw_sock)) < 0) {
+			if (rc == -ECONNRESET) {
+				LOGD("Framework disconnected");
+				close(fw_sock);
+				break;
+			} else {
+				LOGE("Error processing framework command (%s)", strerror(errno));
+			}
+		}//process frame work command
+	}//while
+
+	return (void*)0;
+}
--- jni/src/hardware_bridge.h
+++ jni/src/hardware_bridge.h
@@ -9,8 +9,8 @@ extern int enable_debug;
 #define  LOG_TAG    "hardware_bridge"
 #define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
 #define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
-//#define  LOGD(...)  { if (enable_debug) __android_log_print(ANDROID_LOG_DEBUG,LOG_TAG,__VA_ARGS__); }
-#define  LOGD(...)  { __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__); }
+#define  LOGD(...)  { if (enable_debug) __android_log_print(ANDROID_LOG_DEBUG,LOG_TAG,__VA_ARGS__); }
+//#define  LOGD(...)  { __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__); }
 
 
 #define ANDROID_SOCKET_ENV_PREFIX   "ANDROID_SOCKET_"
